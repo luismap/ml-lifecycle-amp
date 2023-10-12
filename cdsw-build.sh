416bd51ba20248fb8aeece62f4c46b2160824736
luis.map <luismap2508@gmail.com>
2023-10-12 15:49:40 +0200

    [feat] adding cdsw create script

---
+++ cdsw-build.sh
@@ -0,0 +1,9 @@
+
+# Example build script used to customize the engine 
+# environment for Experiments in CML
+
+# This file must be moved to the project's root
+# directory for CML to detect it
+
+pip3 install -U joblib scikit-learn
+#Rscript -e "install.packages(repos='https://cloud.r-project.org', c('readr', 'rsample', 'parsnip', 'yardstick'))"
